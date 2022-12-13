bc05d066017da7cbca56fdce89cae42c4d81e3d1
jrojasnun4 <jrojasnun4@brynmawr.edu>
2022-12-13 14:26:27 -0500

    updated

--- User_Interface.py
+++ User_Interface.py
@@ -1,36 +1,43 @@
-""""This file is the user interface!"""""
+""""
+Go to login file!!! this file 
+This file is the user interface!
+"""""
 import csv
+
+""""
+This part of the code collect the information of the user
+"""""
 
 
 def data_input() -> dict:
     info = {}
 
-    Id = 1
-    with open("Data-information.csv") as f:
+    Id = 1  # the identifier
+    with open("Data-information.csv") as f:   # Open the file
         reader = csv.reader(f, delimiter=',', skipinitialspace=True)
         ids = []
         for line in reader:
             if line[0] not in ids:
                 ids.append(line[0])
-    for number in range(len(ids[:-1])):
-        ids[number + 1] = int(number + 1)
-    max_id = max(ids[1::])
-    Id = max_id + 1
-    info['ID'] = Id
+    for number in range(len(ids[:-1])):  # check each number in the list
+        ids[number + 1] = int(number + 1)  # Convert the str into an int and add 1
+    max_id = max(ids[1::])  # Look what is the biggest number (the last id)
+    Id = max_id + 1  # Add one to the last one
+    info['ID'] = Id  # assign that id to the new user
 
     name = ""
     while name == "":  # this part star the while loop
-        name = str(input("Insert your Name \n"))
+        name = str(input("Insert your Name \n"))  # take the name of the user
         if name == "":
             print("Sorry you have to enter your name")
-    info['Name'] = name
+    info['Name'] = name  # record the name into the dict
 
     last_name = ""
     while last_name == "":  # this part star the while loop
         last_name = str(input("Insert your Last Name \n"))
         if last_name == "":
             print("Sorry you have to enter your Last name")
-    info['Last Name'] = last_name
+    info['Last Name'] = last_name   # record the last name into the dict
 
     # Birthdate
     print("Please insert your Birthdate")
@@ -56,8 +63,8 @@ def data_input() -> dict:
         elif day <= 0 or day > 31:
             print("Remember days go from 1-31")
 
-    birthdate = (year, month, day)
-    info['Birthdate'] = birthdate
+    birthdate = (year, month, day)  # put the year, month, day into a list
+    info['Birthdate'] = birthdate  # insert it into the dict
 
     # Address
     direction = ""
@@ -81,8 +88,8 @@ def data_input() -> dict:
         zip_code = str(zip_code)
         if len(zip_code) != 5:
             print("Sorry you have to fill this field (Zip-codes are 5 digits long)")
-    address = (direction, city, state, zip_code)
-    info['Address'] = address
+    address = (direction, city, state, zip_code)  # put the direction, city, state and zip_code into a list
+    info['Address'] = address  # insert it into the dict
 
     # LOGIN ACCOUNT
     username = ""
@@ -97,25 +104,25 @@ def data_input() -> dict:
         users = []
         for row in reader:
             if row[4] not in users:
-                users.append(row[4])
+                users.append(row[4])  # Take all the usernames before registered and put it in a list
 
     while exist == "a":
-        if username in users:
+        if username in users:  # check if the username already exist
             print("Username already exist try other one")
             username = input("Create another username \n")
         else:
             exist = "l"
 
-    info['User-Name'] = username
+    info['User-Name'] = username  # put the username in the dict
 
     password = "1"
     while len(password) < 5:  # this part star the while loop
         password = str(input("Insert Create Password \n"))
         if len(password) < 5:
             print("Sorry your password have to be greater than 5 long")
-    info['Password'] = password
+    info['Password'] = password  # put the password in the dict
 
-    """Encrypt the data"""
+    """Encrypt the data and imported to the file to keep the data encrypted"""
 
     from Enigma_Encryption import ENIGMA
     with open("Encrypt-data.csv", "a+") as file_object:
@@ -127,25 +134,30 @@ def data_input() -> dict:
             file_object.write("\n")
         # Append text at the end of file
         Id = str(Id) + ", "
-        file_object.write(Id)
-        already_in = [Id]
-        to_encrypt = [name.upper(), last_name.upper(), direction.upper(), password.upper()]
+        file_object.write(Id)  # write the id to identify the user
+        already_in = [Id]  # a list that make sure does not have repeat things
+        to_encrypt = [name.upper(), last_name.upper(), direction.upper(), password.upper()]  # things to encrypt
         for item in to_encrypt:
-            cipher_text = ""
+            cipher_text = ""  # make a new string for each item
             for letter in item:
                 if letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890":
-                    cipher_text = cipher_text + ENIGMA.encipher(letter)
+                    cipher_text = cipher_text + ENIGMA.encipher(letter)  # use the enigma function from the other file to encrypt
                 else:
-                    cipher_text = cipher_text + letter
-            if item != password.upper():
+                    cipher_text = cipher_text + letter  # keep the spaces and symbols
+            if item != password.upper():  # for every string add a space and "," to separate them
                 cipher_text = cipher_text + ", "
             if cipher_text not in already_in:
-                file_object.write(cipher_text)
+                file_object.write(cipher_text)  # add the string to the file
                 already_in.append(cipher_text)
             else:
                 hi = 0
 
     return info
+
+
+"""""
+Import the original data to the record file
+"""""
 
 
 def insert_info():
